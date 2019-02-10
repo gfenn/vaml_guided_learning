dd60b7b085aa58c2b7a4d4244962202bce647a43
Grant Fennessy <grant.fennessy@gmail.com>
2019-02-10 14:51:36 -0600

    Added exponentially weighted moving average line as well.

--- www/demo.js
+++ www/demo.js
@@ -5,14 +5,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
    fetch(episode_api)
      .then(function(response) { return response.json(); })
      .then(function(data) {
+        apply_ewma(data.steps, 'reward', 0.1)
         drawChart(data.steps);
      })
 });
 
+
+function apply_ewma(data, field, beta) {
+    var ewma = data[0][field]
+    data.forEach(function(z) {
+        ewma = z[field] * beta + ewma * (1 - beta)
+        z.ewma = ewma
+    })
+}
+
+
 function drawChart(data) {
     // Set up shape of SVG
     var svgWidth = 600, svgHeight = 400;
-    var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    var margin = { top: 20, right: 20, bottom: 50, left: 50 };
     var width = svgWidth - margin.left - margin.right;
     var height = svgHeight - margin.top - margin.bottom;
     var svg = d3.select('svg')
@@ -30,24 +41,33 @@ function drawChart(data) {
     var line = d3.line()
         .x(function(d) { return x(d.step)})
         .y(function(d) { return y(d.reward)})
+    var line_ewma = d3.line()
+        .x(function(d) { return x(d.step)})
+        .y(function(d) { return y(d.ewma)})
     x.domain(d3.extent(data, function(d) { return d.step }));
     y.domain(d3.extent(data, function(d) { return d.reward }));
 
     g.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x))
-        .select(".domain")
-        .remove();
+        .append("text")
+        .attr("fill", "#000")
+        .attr('font-size', '14px')
+        .attr('x', width / 2)
+        .attr("y", 36)
+        .attr("text-anchor", "middle")
+        .text("Step");
 
     g.append("g")
         .call(d3.axisLeft(y))
         .append("text")
         .attr("fill", "#000")
+        .attr('font-size', '14px')
         .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "0.71em")
-        .attr("text-anchor", "end")
-        .text("Reward");
+        .attr("x", -height/2)
+        .attr('y', -30)
+        .attr("text-anchor", "middle")
+        .text("Step Reward");
 
     g.append("path")
         .datum(data)
@@ -57,5 +77,14 @@ function drawChart(data) {
         .attr("stroke-linecap", "round")
         .attr("stroke-width", 1.5)
         .attr("d", line);
+
+    g.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "green")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", line_ewma);
 }
 
